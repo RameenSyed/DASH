318d3ecb5611122ec508e2970315de43a3f5a640
Citrous06 <71228417+Citrous06@users.noreply.github.com>
2024-10-17 21:21:01 -0400

    Not done, simulation a-go

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,32 @@
 #include "data-manager.h"
 #include "display-manager.h"
 
+
+
+#include "lvgl.h"
+#include "hal/sdl2/app_hal.h"
+
+#include "demos/lv_demos.h"
+#include <demos/widgets/lv_demo_widgets.h>
+
+
 void setup()
 {
 	Serial.begin(9600);
 	Serial.printf("Starting!\n");
 
 	DataManager::init();
+	lv_init();
+
+	hal_setup();
+
+    lv_demo_widgets();
+
 }
 
 void loop()
 {
 	DataManager::update();
-
+	hal_loop();
 	delay(100);
 }
